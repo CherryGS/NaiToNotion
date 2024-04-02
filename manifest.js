cb677d29e67e773eff73bedd9fef2b2028e16838
tickten <tickten@outlook.com>
2024-04-03 05:42:30 +0800

    upd

--- manifest.js
+++ manifest.js
@@ -26,13 +26,17 @@ const manifest = {
   },
   action: {
     default_popup: "src/pages/popup/index.html",
-    default_icon: "icon-34.png"
+    default_icon: "icon-32.png"
   },
   // chrome_url_overrides: {
   //     newtab: 'src/pages/newtab/index.html',
   // },
   icons: {
-    128: "icon-128.png"
+    16: "icon-16.png",
+    32: "icon-32.png",
+    64: "icon-64.png",
+    128: "icon-128.png",
+    256: "icon-256.png"
   },
   content_scripts: [
     {
--- src/pages/options/Options.tsx
+++ src/pages/options/Options.tsx
@@ -1,12 +1,18 @@
-// import "@pages/options/Options.css";
-
 import { Layout, Menu, theme, Typography } from "antd";
 import React, { useState } from "react";
 import { CopyBlock, dracula } from "react-code-blocks";
+import { styled } from "styled-components";
+
+import { ProLayout } from "@ant-design/pro-components";
+import bg from "@assets/img/bg.jpg";
 
 import InputInfo from "./InputInfo";
 
 const { Header, Content, Footer, Sider } = Layout;
+
+const Div = styled.div`
+  background-image: url(${bg});
+`;
 
 const Options: React.FC = () => {
   const {
@@ -20,22 +26,24 @@ const Options: React.FC = () => {
   };
 
   return (
-    <Layout style={{ height: "100%" }}>
-      <Sider breakpoint="lg" collapsedWidth="0" style={{}}>
-        <div className="demo-logo-vertical" />
-        <Menu mode="inline" defaultSelectedKeys={["4"]} style={{ height: "100%", borderRight: 0 }} />
-      </Sider>
-      <Layout>
-        <Header style={{ padding: 0, background: colorBgContainer }} />
-        <Content style={{ margin: "24px 16px 0" }}>
-          <InputInfo set_code={set_code} />
-          <Typography>
-            <CopyBlock language="json" text={code} showLineNumbers={true} theme={dracula} />
-          </Typography>
-        </Content>
-        <Footer style={{ textAlign: "center" }}>Test</Footer>
-      </Layout>
-    </Layout>
+    <Div>
+      <ProLayout style={{ height: "100%", backgroundColor: "#b32121333" }}>
+        <Sider breakpoint="lg" collapsedWidth="0" style={{}}>
+          <div className="demo-logo-vertical" />
+          <Menu mode="inline" defaultSelectedKeys={["4"]} style={{ height: "100%", borderRight: 0 }} />
+        </Sider>
+        <Layout>
+          <Header style={{ padding: 0, background: colorBgContainer }} />
+          <Content style={{ margin: "24px 16px 0" }}>
+            <InputInfo set_code={set_code} />
+            <Typography>
+              <CopyBlock language="json" text={code} showLineNumbers={true} theme={dracula} />
+            </Typography>
+          </Content>
+          <Footer style={{ textAlign: "center" }}>Test</Footer>
+        </Layout>
+      </ProLayout>
+    </Div>
   );
 };
 
